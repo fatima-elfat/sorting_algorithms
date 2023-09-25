27841e4d4c8e5889beddfd7d942c8734a9d9d1c9
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-25 23:05:34 +0100

    fix betty errors

---
+++ 102-counting_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+/**
+ * conting_sort - conting sort algo
+ * @array: array to sort
+ * @size: size of array
+ */
+void conting_sort(int *array, size_t size)
+{
+	int i, j;
+	int *cont, *l;
+
+	if (!array || size < 2)
+		return;
+	for (j = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > j)
+			j = array[i];
+	}
+	cont = malloc(sizeof(int) * (j + 1));
+	if (cont == NULL)
+		return;
+	for (i = 0; i < (j + 1); i++)
+		cont[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		cont[array[i]] += 1;
+	for (i = 0; i < j; i++)
+		cont[i + 1] += cont[i];
+	print_array(cont, j + 1);
+	l = malloc(sizeof(int) * size);
+	if (!l)
+		return;
+	for (i = 0; i < (int)size; i++)
+	{
+		l[cont[array[i]] - 1] = array[i];
+		cont[array[i]] -= 1;
+	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = l[i];
+	free(l);
+	free(cont);
+}
